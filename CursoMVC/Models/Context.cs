3e4c5889108c951ce4daee3b176e685b7c755ae8
Marcos Brandao <marcosv2138@gmail.com>
2021-07-06 00:54:55 -0300

    Novo Projeto de estudo do EntityFrameworkCore

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           //optionsBuilder.UseSqlServer();
+        }
+    }
+}
